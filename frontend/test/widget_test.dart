4e09611b65f2f262b9f83da2f3b6b11916653a2f
Sparkling Water <dylanvh45@gmail.com>
2022-03-10 14:06:30 +0200

    Split front and backend

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mobile_app_frontend/main.dart';
+import 'package:frontend/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
