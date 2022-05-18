1e9c80b4f0c5fce85fb36d6ba6b93ab9cb6ce4eb
DylanvanHeerden <dylanvh45@gmail.com>
2022-05-18 18:50:56 +0200

    Added all user routing to basic pages

--- frontend/lib/user/home/view/user_home_page.dart
+++ frontend/lib/user/home/view/user_home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:itdma3_mobile_app/authentication/authentication.dart';
+import 'package:itdma3_mobile_app/user/bookings/view/view.dart';
+import 'package:itdma3_mobile_app/user/browse_events/view/view.dart';
+import 'package:itdma3_mobile_app/user/edit_profile/view/view.dart';
 
 class UserHomePage extends StatelessWidget {
   const UserHomePage({Key? key}) : super(key: key);
@@ -23,6 +26,24 @@ class UserHomePage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text('Welcome ${state.validatedUser.username}'),
+                  ElevatedButton(
+                    child: const Text('Edit User profile'),
+                    onPressed: () => Navigator.of(context).push(
+                      UserEditProfilePage.route(),
+                    ),
+                  ),
+                  ElevatedButton(
+                    child: const Text('Browse Restaurant events'),
+                    onPressed: () => Navigator.of(context).push(
+                      UserBrowseEventsPage.route(),
+                    ),
+                  ),
+                  ElevatedButton(
+                    child: const Text('View bookings'),
+                    onPressed: () => Navigator.of(context).push(
+                      UserBookingsPage.route(),
+                    ),
+                  ),
                   ElevatedButton(
                     child: const Text('Logout'),
                     onPressed: () {
