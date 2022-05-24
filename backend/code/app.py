a7a7e9d9ddce5f9ccd64b77f41d56d0b520623e9
DylanvanHeerden <dylanvh45@gmail.com>
2022-05-24 22:34:31 +0200

    Added imports

--- backend/code/app.py
+++ backend/code/app.py
@@ -1,10 +1,7 @@
 from flask import Flask
 from flask_restful import Api
-
-
-app = Flask(__name__)
-
-
-if __name__ == "__main__":
-    db.init_app(app)
-    app.run(port=5000, debug=True, host='127.0.0.1')
+from resources.customer import Customer, CustomerLogin, CustomerSignup, CustomerBookedEvents, CustomerAvailableEvents, CustomerList
+from resources.restaurant import Restaurant, RestaurantLogin, RestaurantSignup, RestaurantEvents, RestaurantList
+from resources.event import Event, AddEvent, RestaurantEventBookings, EventList
+from resources.booking import Booking, AddBooking, BookingList
+from db import db
