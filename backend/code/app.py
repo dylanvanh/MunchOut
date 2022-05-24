d92a7708dcfd8cde451768037e706ddf832b6bb8
DylanvanHeerden <dylanvh45@gmail.com>
2022-05-24 22:35:14 +0200

    Added connection to mysql db named itmda3

--- backend/code/app.py
+++ backend/code/app.py
@@ -5,3 +5,14 @@ from resources.restaurant import Restaurant, RestaurantLogin, RestaurantSignup, 
 from resources.event import Event, AddEvent, RestaurantEventBookings, EventList
 from resources.booking import Booking, AddBooking, BookingList
 from db import db
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://admin:root@localhost/itmda3"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'dylan'
+api = Api(app)
+
+
+if __name__ == "__main__":
+    db.init_app(app)
+    app.run(port=5000, debug=True, host='127.0.0.1')
