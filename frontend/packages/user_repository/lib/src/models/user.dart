87aa0e71700f871344c79555055d01c8aa8b5027
DylanvanHeerden <dylanvh45@gmail.com>
2022-05-25 13:17:35 +0200

    Added user model comment descriptions

--- frontend/packages/user_repository/lib/src/models/user.dart
+++ frontend/packages/user_repository/lib/src/models/user.dart
@@ -7,28 +7,56 @@ enum AuthenticationStatus {
   authenticated,
 }
 
+/// User types that are possible for a logged in user
+enum UserType {
+  ///set when customer user type
+  customer,
+
+  /// set when restaurant user type
+  restaurant,
+}
+
 /// User model
 class User {
   ///User contructor
   ///User auth status intially set to unauthenticated
   const User({
-    this.username,
-    this.authToken,
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.phoneNumber,
+    this.restaurantImageUrl, // only gets a value if type =  restaurant user
+    this.restaurantDescription, // only gets a value if type =  restaurant user
+    required this.userType,
     this.authStatus = AuthenticationStatus.unauthenticated,
   });
 
-  /// Unique username for the user
-  /// Used as a primary key
-  final String? username;
+  /// user id created by flask
+  final int id;
 
-  /// Unique auth token for the user
-  final String? authToken;
+  /// name entered by the user
+  final String name;
 
-  ///status of the user
+  /// username that is used for signin
+  final String username;
+
+  /// phoneNumber for contact details
+  final String phoneNumber;
+
+  /// image url of the restaurant for display
+  final String? restaurantImageUrl;
+
+  /// short description of the restuarant
+  final String? restaurantDescription;
+
+  /// Type of user (either customer / restaurant)
+  final UserType userType;
+
+  ///Authentication status of the user
   final AuthenticationStatus authStatus;
 
   @override
   String toString() {
-    return 'Username : $username - AuthToken : $authToken';
+    return 'Base user details : $id , $name , $username , $userType';
   }
 }
