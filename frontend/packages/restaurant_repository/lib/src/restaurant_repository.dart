62e01b250a53c79a8adc498f31f3b065dcc64fc5
DylanvanHeerden <dylanvh45@gmail.com>
2022-05-26 16:57:22 +0200

    Created restaurant_repository

---
+++ frontend/packages/restaurant_repository/lib/src/restaurant_repository.dart
@@ -0,0 +1,7 @@
+/// {@template restaurant_repository}
+/// A repository that handles restaurant requests.
+/// {@endtemplate}
+class RestaurantRepository {
+  /// {@macro restaurant_repository}
+  const RestaurantRepository();
+}
