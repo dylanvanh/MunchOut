60b2e28a5a69d2c9d61bd002c2c33d3747385bd1
DylanvanHeerden <dylanvh45@gmail.com>
2022-05-27 01:33:50 +0200

    Implemented Edit restaurant profile

--- frontend/packages/restaurant_repository/lib/restaurant_repository.dart
+++ frontend/packages/restaurant_repository/lib/restaurant_repository.dart
@@ -1,3 +1,4 @@
 library restaurant_repository;
 
+export './src/models/models.dart';
 export 'src/restaurant_repository.dart';
--- frontend/packages/restaurant_repository/lib/src/restaurant_repository.dart
+++ frontend/packages/restaurant_repository/lib/src/restaurant_repository.dart
@@ -2,8 +2,13 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flask_api/flask_api.dart';
+import 'package:restaurant_repository/restaurant_repository.dart';
 
+/// Thrown when [addEvent] encounters an error
 class AddEventException implements Exception {}
+
+/// Thrown when [getRestaurantDetails] encounters an error
+class FetchRestaurantDetailsException implements Exception {}
 
 class RestaurantRepository {
   /// {@macro restaurant_repository}
@@ -32,4 +37,61 @@ class RestaurantRepository {
       throw AddEventException();
     }
   }
+
+  /// Returns restaurant object
+  ///
+  /// Throws a [AddEventException] if an error occurs
+  Future<Restaurant> getUserDetails({
+    required int restaurantId,
+  }) async {
+    try {
+      final response = await _flaskApi.fetchRestaurantUserDetails(
+        restaurantId: restaurantId,
+      );
+
+      final decodedResponse = jsonDecode(response) as Map<String, dynamic>;
+
+      return Restaurant.fromJson(decodedResponse);
+    } on Exception {
+      throw FetchRestaurantDetailsException();
+    }
+  }
+
+  /// Returns updated restaurant object
+  ///
+  /// Throws a [AddEventException] if an error occurs
+  Future<Restaurant> updateUserDetails({
+    required int restaurantId,
+    required String name,
+    required String password,
+    required String phoneNumber,
+    required String description,
+    required String imageUrl,
+  }) async {
+    try {
+      final response = await _flaskApi.updateRestaurantDetails(
+        restaurantId: restaurantId,
+        name: name,
+        password: password,
+        phoneNumber: phoneNumber,
+        description: description,
+        imageUrl: imageUrl,
+      );
+
+      final decodedResponse = jsonDecode(response) as Map<String, dynamic>;
+
+      return Restaurant.fromJson(decodedResponse);
+    } on Exception {
+      throw FetchRestaurantDetailsException();
+    }
+  }
+
+  // Future<void> main() async {
+  //   final _restaurantRepository = RestaurantRepository();
+
+  //   // getRestaurantUserDetails
+  //   final restaurantDetailsObject =
+  //       await _restaurantRepository.getUserDetails(restaurantId: 5);
+  //   print(restaurantDetailsObject.name);
+  // }
 }
