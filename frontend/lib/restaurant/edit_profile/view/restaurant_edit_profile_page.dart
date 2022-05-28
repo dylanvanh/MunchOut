07162e52afef812f455cd424ec2cce8248f1ad18
DylanvanHeerden <dylanvh45@gmail.com>
2022-05-28 18:18:34 +0200

    Fixed minor highlighted problems

--- frontend/lib/restaurant/edit_profile/view/restaurant_edit_profile_page.dart
+++ frontend/lib/restaurant/edit_profile/view/restaurant_edit_profile_page.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:itdma3_mobile_app/restaurant/edit_profile/edit_profile.dart';
 import 'package:itdma3_mobile_app/restaurant/edit_profile/view/restaurant_edit_profile_form.dart';
 import 'package:restaurant_repository/restaurant_repository.dart';
 import 'package:user_repository/user_repository.dart';
-
-import '../edit_profile.dart';
 
 /// Edit Restaurant screen form , allows the Restaurant to change their
 /// name,password & phonenumber & imageUrl
--- frontend/packages/user_repository/test/src/user_repository_test.dart
+++ frontend/packages/user_repository/test/src/user_repository_test.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:user_repository/user_repository.dart';
 import 'package:test/test.dart';
+import 'package:user_repository/user_repository.dart';
 
 void main() {
   group('AuthRepository', () {
