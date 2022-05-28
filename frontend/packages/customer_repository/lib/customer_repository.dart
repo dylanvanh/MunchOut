83318b058bba361c69c923363abbc41937980450
DylanvanHeerden <dylanvh45@gmail.com>
2022-05-28 19:34:39 +0200

    Created customer_repository

---
+++ frontend/packages/customer_repository/lib/customer_repository.dart
@@ -0,0 +1,3 @@
+library customer_repository;
+
+export 'src/customer_repository.dart';
---
+++ frontend/packages/customer_repository/lib/src/customer_repository.dart
@@ -0,0 +1,7 @@
+/// {@template customer_repository}
+/// A repository that handles customer requests.
+/// {@endtemplate}
+class CustomerRepository {
+  /// {@macro customer_repository}
+  const CustomerRepository();
+}
