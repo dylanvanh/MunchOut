57c697c73564e6619df3964300ca9d5090dae2b1
DylanvanHeerden <dylanvh45@gmail.com>
2022-06-03 02:09:59 +0200

    Implemented Customer loginpage design

--- frontend/lib/customer/login/view/customer_login_page.dart
+++ frontend/lib/customer/login/view/customer_login_page.dart
@@ -1,35 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:itdma3_mobile_app/customer/login/login.dart';
+import 'package:itdma3_mobile_app/customer/signup/view/view.dart';
 import 'package:user_repository/user_repository.dart';
 
 class CustomerLoginPage extends StatelessWidget {
-  const CustomerLoginPage({Key? key}) : super(key: key);
+  CustomerLoginPage({Key? key}) : super(key: key);
 
   static Route<CustomerLoginPage> route() {
     return MaterialPageRoute(
-      builder: (context) => const CustomerLoginPage(),
+      builder: (context) => CustomerLoginPage(),
     );
   }
+
+  final Color gradientTopLeft = const Color.fromRGBO(62, 55, 96, 1);
+  final Color gradientBottomRight = const Color.fromRGBO(22, 98, 157, 1);
+  final Color logoBackground = const Color.fromRGBO(208, 208, 208, 100);
+  final Color textColor = const Color.fromRGBO(27, 92, 151, 1);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Customer Login',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.black,
-      ),
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Image.asset(
-            'assets/restaurant.jpg',
-            fit: BoxFit.cover,
-            color: Colors.black.withOpacity(0.7),
-            colorBlendMode: BlendMode.darken,
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 10,
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(
+                  Icons.arrow_back,
+                ),
+              ),
+            ],
+          ),
+          // SizedBox(
+          //   height: MediaQuery.of(context).size.height / 6,
+          // ),
+          Container(
+            width: MediaQuery.of(context).size.height / 4,
+            decoration: BoxDecoration(
+              color: logoBackground,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(
+                  100,
+                ),
+              ),
+            ),
+            child: Image.asset('assets/logo.png'),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 25,
+          ),
+          Text(
+            'Customer Login',
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.w600,
+              color: textColor,
+            ),
           ),
           Padding(
             padding: const EdgeInsets.all(12),
@@ -42,6 +75,30 @@ class CustomerLoginPage extends StatelessWidget {
               },
               child: const CustomerLoginForm(),
             ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    CustomerSignupPage.route(),
+                  );
+                },
+                child: Row(
+                  children: const [
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    Text(
+                      ' Sign up',
+                      style: TextStyle(color: Color.fromARGB(171, 73, 10, 209)),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
         ],
       ),
